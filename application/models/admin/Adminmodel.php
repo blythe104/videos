57d78e24850d3030f4efe3809f3fe191a9338f2c
wuyuer0220 <wuyuer0220@sina.com>
2016-08-20 15:54:15 +0800

    add adminmodel

---
+++ application/models/admin/Adminmodel.php
@@ -0,0 +1,82 @@
+<?php
+class adminModel extends CI_Model{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 根据条件获取管理员信息
+     * @param $condition
+     * @return array
+     */
+    public function getAdmin($condition)
+    {
+        $this->parseParameter($condition);
+        $result =  $this->db->select('*')
+                        ->from('vadmin')
+                        ->get()
+                        ->result_array();
+        $result =  empty($result) ? array() : $result;
+        return $result;
+    }
+
+    /**
+     * 获取普通条件下的管理员数据
+     * @param $page
+     * @param $count
+     * @return array
+     */
+    public function getCommonAdmin($page = 1,$count = 15)
+    {
+        $condition =  array(
+            'where' => array(
+                'status' => 1
+            ),
+            'limit' => array(
+                'limit' => $count,
+                'offset' => ($page -1)*$count
+            ),
+            'order' => 'create_time desc'
+        );
+        return $this->getAdmin($condition);
+    }
+
+
+    /**
+     * 初始化参数信息
+     * @param $condition
+     */
+    public function parseParameter($condition)
+    {
+        foreach($condition as $key => $value)
+        {
+            if($key == "where")
+            {
+                $this->db->where($value);
+            }else if($key == "order")
+            {
+                $this->db->order_by($value);
+            }else if($key == "limit")
+            {
+                $this->db->limit($value['limit'],$value['offset']);
+            }else if($key == 'join')
+            {
+                if(count($value) > 1)
+                {
+                    foreach($value as $k => $v)
+                    {
+                        $this->db->join($v['table'],$v['condition'],$v['direction']);
+                    }
+                }else{
+                    $this->db->join($value['table'],$value['condition'],$value['direction']);
+                }
+
+            }
+        }
+    }
+
+}
+
+?>
+
