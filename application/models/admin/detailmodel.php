857ce4e63dd86af2f50cdb2d7e2ff9340ca3037a
wuyuer0220 <wuyuer0220@sina.com>
2016-08-20 17:50:06 +0800

    add editfiles

--- application/models/admin/detailmodel.php
+++ application/models/admin/detailmodel.php
@@ -1,5 +1,8 @@
 <?php
 class detailModel extends CI_Model{
+
+    private $mainTable = "vdetail";
+    private $slaveSupportTalbe = "vsupportlog";
     /**
      * 构造函数信息
      */
@@ -15,7 +18,7 @@ class detailModel extends CI_Model{
      */
     public function add($data)
     {
-        return	$this->db->insert('vdetail',$data);
+        return	$this->db->insert($this->mainTable,$data);
     }
 
     /**
@@ -25,7 +28,7 @@ class detailModel extends CI_Model{
      */
     public function edit($data)
     {
-        return $this->db->where('vid',$data['vid'])->update('vdetail',$data);
+        return $this->db->where('vid',$data['vid'])->update($this->mainTable,$data);
     }
 
     /**
@@ -35,8 +38,29 @@ class detailModel extends CI_Model{
      */
     public function checkishas($id)
     {
-        $data  = $this->db->where('vid',$id)->get('vdetail')->row_array();
+        $data  = $this->db->where('vid',$id)->get($this->mainTable)->row_array();
         return empty($data) ? 0 : $data;
+    }
+
+    /**
+     * 检测用户是否已经点赞
+     * @param $uid
+     * @return int
+     */
+    public function checkUserisSupport($uid)
+    {
+        $data  = $this->db->where('muid',$uid)->get($this->slaveSupportTalbe)->row_array();
+        return empty($data) ? 0 : $data;
+    }
+
+    /**
+     * 添加点赞日志信息
+     * @param $data
+     * @return mixed
+     */
+    public function addSupportLog($data)
+    {
+        return $this->db->insert($this->slaveSupportTalbe,$data);
     }
 }
 
